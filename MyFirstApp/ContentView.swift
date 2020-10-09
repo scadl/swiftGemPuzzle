fbbbafaff7acb91c896a69f26bb59d691f92d1b8
Alexandr <scad.luncher@gmail.com>
2020-10-09 16:27:19 +0300

    Some cosmetic changes - trying to work with the UI

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -11,14 +11,40 @@ struct ContentView: View {
     @State private var showingAlert = false
     
     var body: some View {
-        Text("Hello, Mark!")
-            .padding()
-        Text("It's Emaulator in Emulator,\n but it works well too :)")
-            .padding()
-        Button(action: {
-            self.showingAlert = true
-        }) {
-            Text("Click me, please!")
+        
+        VStack {
+            
+            Image(systemName: "homekit")
+            Text("Hello, Mark!")
+                .padding(5)
+            
+            Text("It's Emaulator in Emulator,\n but it works well too :)")
+                .foregroundColor(.red)
+                .padding()
+            
+            Button(action: {
+                self.showingAlert = true
+            }) {
+                HStack{
+                    Image(systemName: "exclamationmark.icloud")
+                    Text("Click me, please!")
+                }
+                .foregroundColor(.white)
+                .padding(10)
+            }
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+            .shadow(radius: 15 )
+            .overlay(
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .stroke(Color.white, lineWidth: 3)
+            )
+            
+            Text("And as you can see, \nI can do some basic things with the UI")
+                .multilineTextAlignment(.center)
+                .padding()
+                .font(.footnote)
+                .foregroundColor(.blue)
         }
         
         .alert(isPresented: $showingAlert) {
