3a8aad3979c25bfb89f8746fa2dac387769bc5d3
Admin <>
2020-10-14 15:53:49 +0300

    Working state-based board fill, with controlled view update

--- MyFirstApp/MainBoardView.swift
+++ MyFirstApp/MainBoardView.swift
@@ -7,15 +7,25 @@
 
 import SwiftUI
 
-var turn:Int = 0            // Player turns count
-var nums:[String] = []      // Storage of used number
-var cellNumb = [[String]]()
+var turn:Int = 0                // Player turns count
+var nums:[String] = []          // Storage of used number
+var counter:[Int] = [1,2,3,4]           // Game board cells count
+let cellSize:CGFloat = 70.0             // Game baord cells size
+var shouldUpdate = true
+
+// A matrix for stroring intal values for board
+var cellNumbI = [[String]](
+    repeating: [String](repeating: "", count: counter.count),
+    count: counter.count
+)
 
 struct MainBoardView: View {
     
-    var counter:[Int] = [1,2,3,4]           // Game board cells count
-    let cellSize:CGFloat = 70.0             // Game baord cells size
-    //@State var cellNumb = [[String]]()      // Dictioanry of cells values
+    // Dictioanry of actual cells values
+    @State var cellNumb = [[String]](
+        repeating: [String](repeating: "#", count: counter.count),
+        count: counter.count
+    )
     
     var body: some View {
 
@@ -26,7 +36,6 @@ struct MainBoardView: View {
             ForEach(counter, id:\.self){ numRow in
                 
                 let _ = print("r:"+String(numRow))
-                makeRow(row: numRow)
                 
                 HStack {
                     // Column building cycle
@@ -34,9 +43,9 @@ struct MainBoardView: View {
                         
                         let _ = print("c:"+String(numCol))
                         
-                        let rVal = getRandAndRemove(row: numRow, col: numCol)
+                        getRandAndRemove(row: numRow, col: numCol)
                         
-                        let cVal = rVal
+                        let cVal = cellNumb[numRow-1][numCol-1]
                         let curColor = cVal=="0" ? Color.gray : Color.blue
                         let curNum = cVal=="0" ? " " : cVal
                         BoardCellView(
@@ -44,45 +53,57 @@ struct MainBoardView: View {
                                 cellSize: cellSize,
                                 cellColor: curColor,
                                 onTo: {
-                                    print("click ")
+                                    print("click "+cVal)
                                 }
                         )
                         
-                        let _ = print(cellNumb)
+                        
+                        
                     }
                 }.padding(2)
+                  
             }
-        
+            
+            if !(cellNumbI==cellNumb){
+                storeBoardValues()
+            }
+            let _ = print(cellNumbI)
+            let _ = print(cellNumb)
+            
         }
+       
         
     }
     
     // A function for generating non-repeating random numbers
-    func getRandAndRemove(row:Int,col:Int)->String{
+    func getRandAndRemove(row:Int,col:Int)->some View{
         
         var cur = 0
                             
-        
         while((nums.firstIndex(of: String(cur))) != nil){
             cur = Int.random(in: 1...Int(pow(Double(counter.count),2))-1)
         }
         nums.append(String(cur))
         
+        cellNumbI[row-1][col-1] = String(cur)
         
-        //if(cellNumb[row][col]==nil) {
-        cellNumb[row-1].append(String(cur))
-        //}
-
-        return String(cur)
-    }
-    
-    func makeRow(row:Int)->some View{
-        cellNumb.append([])
         return EmptyView()
     }
     
+    
     func cellClick(){
         
+    }
+    
+    func storeBoardValues()->some View{
+        
+        DispatchQueue.main.async {
+            nums.removeAll()
+            cellNumb = cellNumbI
+        }
+        
+        shouldUpdate = false
+        return EmptyView()
     }
 }
 
--- MyFirstApp/MainMenuView.swift
+++ MyFirstApp/MainMenuView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MainMenuView: View {
     
     @State private var showingAlert:Bool = false
-    var icoPadding:CGFloat = 1.0
+    var icoPadding:CGFloat = 0.1
     var btnHeigh:CGFloat = 200.0
     var btnRadius:CGFloat = 15.0
     var uiPadding:CGFloat = 5.0
